65e1ee972910f879fe1f1998dc42d450b244eb13
Rohit Panjwani <iamrohit.panjwani@gmail.com>
2020-06-06 18:41:55 +0530

    bin component showing data

--- src/components/bin.js
+++ src/components/bin.js
@@ -1,11 +1,93 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import AddNewTodo from './addNewTodo';
+import { makeStyles } from '@material-ui/core/styles';
+import { Typography } from '@material-ui/core';
+import Todo from './todo';
+import axios from 'axios';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+import { connect } from 'react-redux';
 
-function Bin() {
+
+function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        padding: '5%'
+    },
+    heading: {
+    [theme.breakpoints.up("md")]: {
+        fontSize: "1.2rem"
+        },
+        margin: '25px 0px'
+    },
+    todos: {
+        display: 'grid',
+        gridTemplateColumns: '23% 23% 23% 23%',
+        columnGap: '2%',
+        rowGap: '15px'
+    }
+}))
+
+function Bin(props) {
+    const classes = useStyles();
+
+    const [state, setState] = React.useState({
+        snackbar: {
+            show: false,
+            msg: '',
+            color: 'success'
+        }
+    })
+
+    const handleChange = (e, key, prop) => {
+        let value = e.target.value;
+        setState(state => ({
+            ...state,
+            [key]: {
+                ...state[key],
+                [prop]: value
+            }
+        }))
+    }
+
     return (
-        <div>
-            Bin
-        </div>
+        <Grid container className={classes.root}>
+            <Grid item xs={12}><Grid container>
+                    <Grid item xs={12}>
+                        <Typography variant="body1" className={classes.heading}>Deleted Tasks</Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                        {props.bin.length ? <div className={classes.todos}>
+                            {props.bin.map((task, index) => (
+                                <Todo index={index} task={task} key={task._id} bin={true}/>
+                            ))}
+                        </div> : 
+                        <Typography variant="body2">No tasks is bin</Typography>}
+                    </Grid>
+                    <Grid item xs={12} style={{margin: '20px 0'}}>
+                        <Typography variant="body2">Deleted tasks appear here until browser clears them out</Typography>
+                    </Grid>
+                </Grid>
+            </Grid>
+            <Snackbar open={state.snackbar.show} autoHideDuration={6000} onClose={() => handleChange({target: {value: false}}, 'snackbar', 'show')}>
+                <Alert onClose={() => handleChange({target: {value: false}}, 'title')} severity={state.snackbar.color}>
+                    {state.snackbar.msg}
+                </Alert>
+            </Snackbar>
+        </Grid>
     );
 }
 
-export default Bin;
+const mapStateToProps = (state) => ({
+    bin: state.bin,
+})
+
+const mapDispatchToProps = (dispatch) => ({
+})
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Bin);
