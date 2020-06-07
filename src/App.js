71d7adef76a2f7e0432720ac0108b36985cbc25f
Rohit Panjwani <iamrohit.panjwani@gmail.com>
2020-06-07 21:35:25 +0530

    Added navbar for small devices

--- src/App.js
+++ src/App.js
@@ -1,20 +1,47 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid'
 import { Route, Switch } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
 import './App.css';
 
 import Navbar from './components/navbar';
+import TopNavbar from './components/topnavbar';
 import Login from './components/login';
 import Home from './components/home';
 import Settings from './components/settings';
 import Archive from './components/archive';
 import Bin from './components/bin';
 
+const useStyles = makeStyles(theme => ({
+  navbarShort: {
+      [theme.breakpoints.up('xs')] : {
+        display:'none'
+      },
+      [theme.breakpoints.down('xs')] : {
+        display:'block'
+      }
+  },
+  navbarLong: {
+    [theme.breakpoints.down('xs')] : {
+      display:'none'
+    },
+    [theme.breakpoints.up('md')] : {
+      display:'block'
+    }
+  }
+}))
+
 function App() {
+
+  const classes = useStyles();
+
   return (
     <Grid container style={{height: '100vh'}}>
-      <Grid item xs={3} lg={2}>
-        <Navbar />
+      <Grid item xs={3} lg={2} className={classes.navbarLong}>
+        <Navbar/>
+      </Grid>
+      <Grid xs={12} className={classes.navbarShort}>
+      <TopNavbar/>
       </Grid>
       <Grid item xs={9} lg={10} style={{overflow: 'auto', height: '100%'}}>
         <Switch>%
---
+++ src/components/topnavbar.js
@@ -0,0 +1,154 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import HomeIcon from '@material-ui/icons/Home';
+import VpnKeyIcon from '@material-ui/icons/VpnKey';
+import SettingsIcon from '@material-ui/icons/Settings';
+import DeleteIcon from '@material-ui/icons/Delete';
+import CopyrightIcon from '@material-ui/icons/Copyright';
+import ArchiveIcon from '@material-ui/icons/Archive';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Login from './login'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    color: 'white'
+  },
+  route: {
+      textDecoration: 'none',
+      color: 'black'
+  }
+}));
+
+const routes = [
+    {
+        title: 'Home',
+        route: '/',
+        icon: <HomeIcon fontSize="small" style={{margin: '10px'}}/>
+    },
+    {
+        title: 'Archive',
+        route: '/archive',
+        icon: <ArchiveIcon fontSize="small"  style={{margin: '10px'}}/>
+    },
+    {
+        title: 'Bin',
+        route: '/bin',
+        icon: <DeleteIcon fontSize="small"  style={{margin: '10px'}}/>
+    },
+    {
+        title: 'Settings',
+        route: '/settings',
+        icon: <SettingsIcon fontSize="small"  style={{margin: '10px'}}/>
+    },
+]
+
+function TopNavbar(props) {
+  const classes = useStyles();
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const logout = () => {
+      props.logout();
+      handleClose();
+  }
+
+  const login = () => {
+    handleClickOpen();
+    handleClose();
+  }
+
+  const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleCloseDialog = () => {
+        setOpen(false);
+    };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} onClick={handleClick} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+        >
+            {routes.map(route => 
+            (<Link to={route.route} className={classes.route}>
+                <MenuItem onClick={handleClose}>{route.icon}{route.title}</MenuItem>
+            </Link>)
+            )}
+            {!props.loggedIn ? <MenuItem onClick={login}><VpnKeyIcon  style={{margin: '10px'}}/>Login</MenuItem> :
+            <MenuItem onClick={logout}><ExitToAppIcon  style={{margin: '10px'}}/>Logout</MenuItem>}
+            <MenuItem onClick={handleClose}><CopyrightIcon  style={{margin: '10px'}}/>Rohit Panjwani</MenuItem>
+        </Menu>
+          <Typography variant="h6" className={classes.title}>
+            Rohit Panjwani
+          </Typography>
+        </Toolbar>
+        <Dialog
+                open={open}
+                onClose={handleCloseDialog}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+                maxWidth={"md"}
+            >
+            <DialogContent>
+                <Login close={handleCloseDialog}/>
+            </DialogContent>
+            </Dialog>
+      </AppBar>
+    </div>
+  );
+}
+
+const mapStateToProps = (state) => ({
+    loggedIn: state.loggedIn,
+    username: state.username
+})
+
+const mapDispatchToProps = (dispatch) => ({
+    logout: () => dispatch({type: 'LOGOUT'})
+})
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(TopNavbar);
